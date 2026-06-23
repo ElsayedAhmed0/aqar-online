ee617414bb22f1fb07822fe8d2e6b65c69cfaef1
sayed <sayed.khalil@beetelware.net>
2026-06-24 00:26:14 +0300

    add initial layout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "TyKS8blYgP3M25I",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
