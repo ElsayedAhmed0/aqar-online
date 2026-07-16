9c72da100f1158edac59fbd16cd23cbce3f4b3f8
sayed <sayed.khalil@beetelware.net>
2026-07-16 20:27:32 +0300

    add filter

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,3 @@
-
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
@@ -6,6 +5,17 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: [
+        '/*/admin',
+        '/*/dashboard',
+        '/*/login',
+        '/*/register',
+        '/*/forgot-password',
+        '/*/reset-password',
+        '/*/profile',
+        '/*/wishlist',
+        '/*/add-listing',
+      ],
     },
     sitemap: 'https://www.aqqaronline.com/sitemap.xml',
   }
